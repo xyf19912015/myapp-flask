b0851adec1f5361c4e138f168f61aff5a027b916
xyf19912015 <154675818+xyf19912015@users.noreply.github.com>
2024-11-26 15:54:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ scaler, best_xgb, feature_names, best_threshold, best_youden_index = train_model
 @app.route('/')
 def home():
     annotations = {
-        'Num of involved CAs': 'Number of Involved Coronary Arteries, n',
+        'No of involved CAs': 'Number of Involved Coronary Arteries, n',
         'Zmax of initial CALs': 'Zmax of initial CALs',
         'Age': 'Age of onset, years',
         'IGT': 'Day of first time to use IVIG, day',
